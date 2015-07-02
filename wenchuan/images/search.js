96f2966098b8935b4413d3e0b8763a4917b417d0
hhjay <sunhhjay@gmail.com>
2015-07-02 09:00:48 +0800

    数据整合

--- wenchuan/images/search.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  $("#searchForm").attr("action","http://www.tsinghua.edu.cn/easyDig/Default.jsp");
-  $("#searchVpath").attr("value","http://www.tsinghua.edu.cn/publish/th/");
-});
